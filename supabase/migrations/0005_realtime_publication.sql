d7fef25a34348ddb33376aa59280bc114685febf
bezerraluiz-codeform <luiz.a@cartoriocriciuma.com.br>
2025-12-12 15:28:46 -0300

    refactor: Atualização da documentação das novas features

--- supabase/migrations/0005_realtime_publication.sql
+++
@@ -1,8 +0,0 @@
--- Migration: enable Realtime for tasks and workspace_timer_state
--- Objetivo: adicionar tabelas na publicação supabase_realtime para consumo pelo front-end
-
-alter publication supabase_realtime add table public.tasks;
-
-alter publication supabase_realtime add table public.workspace_timer_state;
-
-
